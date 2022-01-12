0e446f07039f79e5613796413438e3e373ecb10c
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-01-12 21:24:44 +0400

    some basic structure with react icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Sidebar } from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+    <div className="grid grid-cols-12 gap-6 px-5 my-16 lg:px-48">
+      <div className="col-span-12 lg:col-span-3 bg-slate-400 rounded-2xl">
+        <Sidebar/>
+      </div>
+      <div className="col-span-12 lg:col-span-9 bg-slate-400 rounded-2xl">
+        <Component {...pageProps} />
+      </div>
+    </div>
+
+  )
 }
 
 export default MyApp
